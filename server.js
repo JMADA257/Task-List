5cafe526ef1bd3596f06e466aded39fe2483a714
JMADA257 <jadams1812@hotmail.com>
2023-09-21 18:29:00 -0600

    Updated js code to include server code!

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const path = require("path");
+const uuid = require("./helpers/uuid");
+const fsUtils = require("./helpers/fsUtils");
+const notesData = require("./db/db.json");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  fsUtils.readFromFile("./db/db.json").then((data) => {
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+  const userNote = {
+    title: title,
+    text: text,
+    id: uuid(),
+  };
+  fsUtils.readAndAppend(userNote, "./db/db.json");
+  console.log(userNote);
+  res.json("Note was added");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listenening at http://localhost:${PORT}`);
+});
