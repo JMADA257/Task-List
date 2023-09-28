31c64a1a2152b0f760ecd28adff740dbdff24516
JMADA257 <jadams1812@hotmail.com>
2023-09-28 09:44:26 -0600

    updated notes to js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+//pulling express
 const express = require("express");
+//pulling the path method from node
 const path = require("path");
+//pulling uuid using a relative path
 const uuid = require("./helpers/uuid");
+//pulling fsutils from a relative path
 const fsUtils = require("./helpers/fsUtils");
+//pulling in the possible route of notesdata using relative path
 const notesData = require("./db/db.json");
+//setting up a port for a port for heroku and localhost to use
 const PORT = process.env.PORT || 3001;
+//the variable app now is linked to all the express methods
 const app = express();
 
 //Middleware
@@ -11,20 +18,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//setting the base home page http://localhost:3001/
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+//setting the base home page http://localhost:3001/notes
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+//setting the base home page http://localhost:3001/api/notes
 app.get("/api/notes", (req, res) => {
   fsUtils.readFromFile("./db/db.json").then((data) => {
     res.json(JSON.parse(data));
   });
 });
 
+//setting the base home page http://localhost:3001/api/notes
 app.post("/api/notes", (req, res) => {
   const { title, text } = req.body;
   const userNote = {
